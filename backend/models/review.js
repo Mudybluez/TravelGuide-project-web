d977605ae4041019b033050befdd7c11067638e7
Mudybluez <madi.kasymov.05@mail.ru>
2025-02-25 03:14:52 +0500

    token bug fixes

--- backend/models/review.js
+++ backend/models/review.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const reviewSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    username: { type: String, required: true, unique: true },
     cityOrCountry: { type: String, required: true },
     rating: { type: Number, required: true, min: 0, max: 5 },
     description: { type: String, required: true },
