b0e5ed065997316ab9ec017a12d0af7a2e588d56
Mudybluez <madi.kasymov.05@mail.ru>
2025-02-25 01:10:58 +0500

    token bug fixes

--- backend/controllers/reviewController.js
+++ backend/controllers/reviewController.js
@@ -7,6 +7,7 @@ exports.getAllReviews = async (req, res) => {
     const { page = 1, limit = 6 } = req.query;
     try {
         const reviews = await Review.find()
+            .populate('user', 'username')
             .limit(limit * 1)
             .skip((page - 1) * limit)
             .exec();
@@ -23,9 +24,14 @@ exports.getAllReviews = async (req, res) => {
 
 // Создание нового отзыва
 exports.createReview = async (req, res) => {
-    const { username, stars, reviewText } = req.body;
+    const { cityOrCountry, rating, description } = req.body;
     try {
-        const newReview = new Review({ username, stars, reviewText });
+        const newReview = new Review({
+            user: req.user.id,
+            cityOrCountry,
+            rating,
+            description,
+        });
         await newReview.save();
         res.status(201).json(newReview);
     } catch (err) {
@@ -36,9 +42,9 @@ exports.createReview = async (req, res) => {
 // Обновление отзыва
 exports.updateReview = async (req, res) => {
     const { id } = req.params;
-    const { username, stars, reviewText } = req.body;
+    const { cityOrCountry, rating, description } = req.body;
     try {
-        const updatedReview = await Review.findByIdAndUpdate(id, { username, stars, reviewText }, { new: true });
+        const updatedReview = await Review.findByIdAndUpdate(id, { cityOrCountry, rating, description }, { new: true });
         res.status(200).json(updatedReview);
     } catch (err) {
         res.status(400).json({ error: err.message });
--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { getAllReviews, createReview, updateReview, deleteReview } = require('../controllers/reviewController');
+const { verifyToken } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.get('/', getAllReviews);
-router.post('/', createReview);
-router.put('/:id', updateReview);
-router.delete('/:id', deleteReview);
+router.post('/', verifyToken, createReview);
+router.put('/:id', verifyToken, updateReview);
+router.delete('/:id', verifyToken, deleteReview);
 
 module.exports = router;
